cff6932189c98b0c9dc2cd3241b0641f747c14e0
Taran Mittal <taranmittal09@gmail.com>
2025-09-23 12:46:12 +1000

    Update +page.svelte

--- src/routes/sydney/+page.svelte
+++ src/routes/sydney/+page.svelte
@@ -23,7 +23,7 @@
 		{ image: "/sydney/oshw.png", name: "oshw", url: "https://oshwlab.com/" },
 		{ image: "/sydney/easyeda.png", name: "easyeda", url: "https://easyeda.com/" },
 		{ image: "/sydney/logo_horizontal_black_registered.png", name: "janestreet", url: "https://www.janestreet.com/" },
-		{ image: "/sydney/Sstickercofixed.png", name: "StickerCo", url: "https://www.stickerco.au/" },
+		{ image: "/sydney/stickercofixed.png", name: "StickerCo", url: "https://www.stickerco.au/" },
 		{ image: "/sydney/insertcoinfixed.png", name: "Insert Coin", url: "https://www.insertcoinclothing.com/" },
 		{ image: "/sydney/fluffe.png", name: "Fluffe", url: "https://www.fluffe.com/" },
 
