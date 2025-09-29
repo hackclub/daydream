1dcb1559b1668ce6104a79abeeff96f4225be0d9
Taran Mittal <taranmittal09@gmail.com>
2025-09-29 18:59:49 +1000

    add sponsors

--- src/routes/sydney/+page.svelte
+++ src/routes/sydney/+page.svelte
@@ -19,6 +19,10 @@
 	const sponsorsEnabled = true; // Set to false to hide the entire sponsors section
 	const sponsors = [
 		{ image: "/sydney/accel2.png", name: "AccelRT", url: "https://accelrt.net" },
+		{ image: "/sydney/emu.png", name: "Emu Robotics", url: "https://first.unsw.edu.au/" },
+		{ image: "/sydney/SparkCubing.png", name: "Spark Cubing", url: "https://sparkcubing.com.au" },
+		{ image: "/sydney/SparkEdu.png", name: "Spark Education", url: "https://www.sparkedu.com/en-us" },
+		{ image: "/sydney/tinkertank.png", name: "TinkerTank", url: "https://www.tinkertank.rocks/" },
 		{ image: "/sydney/xyz.png", name: ".xyz", url: "https://gen.xyz" },
 		{ image: "/sydney/jlcpcb.png", name: "jlcpcb", url: "https://jlcpcb.com/" },
 		{ image: "/sydney/oshw.png", name: "oshw", url: "https://oshwlab.com/" },
